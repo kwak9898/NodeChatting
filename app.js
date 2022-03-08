f34db99da0dddbea25cee709b3ea0889ca4f721b
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-03-08 15:45:46 +0900

    socket.io 적용

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const port = 3000
 const app = express()
-const http = require('http')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    socket.on('request_message', (msg) => {
+        io.emit('response_message', msg)
+    })
+
+    socket.on('disconnect', async () => {
+        console.log('user disconnected')
+    })
 })
 
 //error
