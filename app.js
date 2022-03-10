49c74367bedb2c98bd4cc059a0143b483c2202ad
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-03-10 16:27:57 +0900

    서버 수정

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const socket = require('socket.io');
+const express = require('express');
+const app = express();
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    fs.readFile('index.html', (error, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data)
+    });
+});
+
+server.listen(3000, () => {
+    console.log('3000번 포트 연결 성공!!!')
+})
